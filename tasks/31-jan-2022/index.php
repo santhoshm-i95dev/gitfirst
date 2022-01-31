012a4b73746cab9e2f39e64734ad07809ff7b9a5
santhoshm-i95dev <santhosh.m@i95dev.com>
2022-01-31 18:58:50 +0530

    31 jan 2022

---
+++ tasks/31-jan-2022/index.php
@@ -0,0 +1,46 @@
+<?php
+#establishing a database connection
+
+$connection = new  mysqli("localhost", "i95dev", "i95dev" , "test");
+
+# if condition will displays if the connection not gets established
+if ($connection->connect_error==true) {
+    echo("Database connection failed: ");
+}
+# else block will displays if the connection gets established
+else {
+    echo "connection  established" ;
+    echo "<br>";
+    $query = ' CREATE TABLE human(
+    PersonID int autoincrement,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255)
+)' ;
+    if ( $connection -> query ($query)){
+        echo "table created"; }
+    else{
+        echo " table not created";} echo "<br>";
+    #insert into the table
+$insertinto = 'insert into Persons(PersonID,LastName,FirstName,Address,city) values(001,"kumar","anthanoy","bangalore","vijaynagar")';
+    $insertinto = 'insert into Persons(PersonID,LastName,FirstName,Address,city) values(001,"kumar","anthanoy","bangalore","vijaynagar")';
+    $insertinto = 'insert into Persons(PersonID,LastName,FirstName,Address,city) values(002,"dd","anthagagfgnoy","bangalore","vijaynagar")';
+    $insertinto = 'insert into Persons(PersonID,LastName,FirstName,Address,city) values(003,"kudddmar","gaga","bangalore","vijaynagar")';
+    $insertinto = 'insert into Persons(PersonID,LastName,FirstName,Address,city) values(004,"kugggmar","anhgfshstyhthanoy","bangalore","vijaynagar")';
+if ($connection->query($insertinto)){
+        echo "the valuee inserted into the table";
+    }
+    else { echo "values not inserted";} echo "<br>";
+
+    #inserting the values into the table human ;
+    $insertintoq = 'insert into human( PersonID ,LastName, FirstName ,Address,City ) values (50,"abc","def","blore","rajblore")';
+
+    if ($connection->query($insertintoq)){
+        echo "the human valuee inserted into the table";
+    }
+    else { echo "human values not inserted";} echo "<br>";
+
+}
+
+?>
